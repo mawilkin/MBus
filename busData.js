d64e9ee782f44cfd1273353e340a6f68b9a01eb1
mawilkin <mawilkin@umich.edu>
2014-07-16 11:59:37 -0400

    added basic geolocation function

--- busData.js
+++ busData.js
@@ -1,6 +1,23 @@
-    $("#refresh").click(function(){
-      console.log('refreshing')
-      var temp = $('#stop-div').html();
-      $('#stop-div').html("")
-      $('#stop-div').html(temp)
-    });
+$("#refresh").click(function(){
+  console.log('refreshing')
+  var temp = $('#stop-div').html();
+  $('#stop-div').html("")
+  $('#stop-div').html(temp)
+});
+
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude); 
+    console.log("Longitude: " + position.coords.longitude); 
+}
+
+getLocation();
